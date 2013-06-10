9b802cefa8ddbb94ffb12819b27d54010e5f99b4
Wank <alltest@volny.cz>
2013-06-10 02:49:32 +0200

    first

---
+++ MvcApplication7/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication7.Models
+{
+    public class UserViewModel
+    {
+        public string leto { get; set; }
+        public string zima { get; set; }
+    }
+}
